5b6efd61292ce74a453d4d18427d41eca2b3e28f
Sergio Marotco <sergiomarotco@gmail.com>
2019-12-04 01:50:44 +0700

    fix

--- PasswordFinder/Properties/AssemblyInfo.cs
+++ PasswordFinder/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PasswordFinder")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/sergiomarotco/MS-office-Password-Finder")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("https://github.com/sergiomarotco")]
 [assembly: AssemblyProduct("PasswordFinder")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
